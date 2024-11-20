0f5d7624c690a8ae24885f6b97b1be6444798108
Markus Staab <markus.staab@redaxo.de>
2024-11-20 16:33:24 +0100

    cleanup bin scripts

--- bin/cross-repo-unused-methods-analyzer.php
+++ bin/cross-repo-unused-methods-analyzer.php
@@ -9,14 +9,13 @@ if (!isset($argv[1])) {
 require_once __DIR__.'/../vendor/autoload.php';
 
 $cmd =
-        realpath(__DIR__.'/../vendor/bin/phpstan')
-        .' analyze '. escapeshellarg($argv[1])
-        .' --configuration '. realpath(__DIR__.'/../cross-repo-unused-methods-analyzer.neon')
-        .'
-        --autoload-file '. realpath(__DIR__.'/bootstrap-fake-collectors.php')
-        // . ' --debug --xdebug'
+    realpath(__DIR__.'/../vendor/bin/phpstan')
+    .' analyze '. escapeshellarg($argv[1])
+    .' --configuration '. escapeshellarg(realpath(__DIR__.'/../cross-repo-unused-methods-analyzer.neon'))
+    .' --autoload-file '. escapeshellarg(realpath(__DIR__.'/bootstrap-fake-collectors.php'))
+    //.' --debug'
+    //.' --xdebug'
 ;
-
 $output = \staabm\CrossRepoUnusedPublic\execCmd($cmd, $stderrOutput, $exitCode);
 
 echo $output;
--- bin/cross-repo-unused-methods-recorder.php
+++ bin/cross-repo-unused-methods-recorder.php
@@ -11,9 +11,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 $cmd =
     realpath(__DIR__.'/../vendor/bin/phpstan')
     .' analyze '. escapeshellarg($argv[1])
-    .' --configuration '. realpath(__DIR__.'/../cross-repo-unused-methods-recorder.neon')
-    .' '
-    // . ' --debug --xdebug'
+    .' --configuration '. escapeshellarg(realpath(__DIR__.'/../cross-repo-unused-methods-recorder.neon'))
+    //.' --debug'
+    //.' --xdebug'
 ;
 $output = \staabm\CrossRepoUnusedPublic\execCmd($cmd, $stderrOutput, $exitCode);
 
