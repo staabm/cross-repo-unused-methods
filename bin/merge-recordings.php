6d16ca4b09400fd36e46186904af17f51a8edd43
Markus Staab <markus.staab@redaxo.de>
2024-11-20 16:33:53 +0100

    Create merge-recordings.php

---
+++ bin/merge-recordings.php
@@ -0,0 +1,54 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+use Nette\Utils\Json;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+if (!isset($argv[1]) || !isset($argv[2])) {
+    fwrite(STDERR, 'Please provide at least 2 path to be merged');
+    exit(1);
+}
+
+$files = [];
+for ($i = 1; $i < count($argv); $i++) {
+    $files[] = $argv[$i];
+}
+
+$mergedJson = [];
+foreach($files as $file) {
+    if (!file_exists($file)) {
+        fwrite(STDERR, sprintf('File %s does not exist', $file));
+        exit(1);
+    }
+
+    $contents = file_get_contents($file);
+    if ($contents === false) {
+        throw new \LogicException();
+    }
+
+    $json = Json::decode($contents, true);
+    $mergedJson = appendArray($mergedJson, $json);
+}
+
+echo Json::encode($mergedJson, true);
+
+function appendArray(array $arr1, array $arr2): array {
+    foreach($arr2 as $key => $value) {
+        if (array_key_exists($key, $arr1)) {
+            if (is_array($value)) {
+                $arr1[$key] = appendArray($arr1[$key], $value);
+            } elseif (is_array($arr1[$key])) {
+                $arr1[$key][] = $value;
+            } elseif (is_array($arr1)) {
+                $arr1[] = $value;
+            } else {
+                throw new LogicException();
+            }
+        } else {
+            $arr1[$key] = $value;
+        }
+    }
+
+    return $arr1;
+}
