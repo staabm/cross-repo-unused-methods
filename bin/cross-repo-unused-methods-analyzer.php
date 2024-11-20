b279f213ef819f0a3fd89f427753ceadcce62009
Markus Staab <markus.staab@redaxo.de>
2024-11-20 16:33:46 +0100

    support passing options to underlying phpstan

--- bin/cross-repo-unused-methods-analyzer.php
+++ bin/cross-repo-unused-methods-analyzer.php
@@ -1,20 +1,24 @@
 #!/usr/bin/env php
 <?php declare(strict_types=1);
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 if (!isset($argv[1])) {
     fwrite(STDERR, 'Please provide the path to the project you want to analyze');
     exit(1);
 }
 
-require_once __DIR__.'/../vendor/autoload.php';
+$moreOptions = [];
+for ($i = 2; $i < count($argv); $i++) {
+    $moreOptions[] = $argv[$i];
+}
 
 $cmd =
     realpath(__DIR__.'/../vendor/bin/phpstan')
     .' analyze '. escapeshellarg($argv[1])
     .' --configuration '. escapeshellarg(realpath(__DIR__.'/../cross-repo-unused-methods-analyzer.neon'))
     .' --autoload-file '. escapeshellarg(realpath(__DIR__ . '/../src/Analyzer/bootstrap-fake-collectors.php'))
-    //.' --debug'
-    //.' --xdebug'
+    .' '. implode(' ', $moreOptions)
 ;
 $output = \staabm\CrossRepoUnusedPublic\execCmd($cmd, $stderrOutput, $exitCode);
 
--- bin/cross-repo-unused-methods-recorder.php
+++ bin/cross-repo-unused-methods-recorder.php
@@ -1,19 +1,23 @@
 #!/usr/bin/env php
 <?php declare(strict_types=1);
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 if (!isset($argv[1])) {
     fwrite(STDERR, 'Please provide the path to the project you want to analyze');
     exit(1);
 }
 
-require_once __DIR__.'/../vendor/autoload.php';
+$moreOptions = [];
+for ($i = 2; $i < count($argv); $i++) {
+    $moreOptions[] = $argv[$i];
+}
 
 $cmd =
     realpath(__DIR__.'/../vendor/bin/phpstan')
     .' analyze '. escapeshellarg($argv[1])
     .' --configuration '. escapeshellarg(realpath(__DIR__.'/../cross-repo-unused-methods-recorder.neon'))
-    //.' --debug'
-    //.' --xdebug'
+    .' '. implode(' ', $moreOptions)
 ;
 $output = \staabm\CrossRepoUnusedPublic\execCmd($cmd, $stderrOutput, $exitCode);
 
