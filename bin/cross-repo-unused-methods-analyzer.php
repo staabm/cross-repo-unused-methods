425b802298bffd3b6fd3ce40a1e50213d60e1614
Markus Staab <markus.staab@redaxo.de>
2024-11-20 16:31:46 +0100

    separate stderr, stdout streams from inner process

--- bin/cross-repo-unused-methods-analyzer.php
+++ bin/cross-repo-unused-methods-analyzer.php
@@ -1,4 +1,11 @@
-<?php
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+require __DIR__ .'/../src/functions.php';
 
 $cmd = __DIR__.'/../vendor/bin/phpstan analyze -c '. realpath(__DIR__.'/../cross-repo-unused-methods-analyzer.neon') .' '. realpath(__DIR__.'/empty.php') .' --debug --xdebug';
-system($cmd);
+$output = \staabm\CrossRepoUnusedPublic\execCmd($cmd, $stderrOutput, $exitCode);
+
+echo $output;
+fwrite(STDERR, $stderrOutput);
+exit($exitCode);
--- bin/cross-repo-unused-methods-recorder.php
+++ bin/cross-repo-unused-methods-recorder.php
@@ -1,10 +1,16 @@
-<?php
+#!/usr/bin/env php
+<?php declare(strict_types=1);
 
 if (!isset($argv[1])) {
-    echo 'Please provide the path to the project you want to analyze';
+    fwrite(STDERR, 'Please provide the path to the project you want to analyze');
     exit(1);
 }
 
+require __DIR__ .'/../src/functions.php';
+
 $cmd = realpath(__DIR__.'/../vendor/bin/phpstan'). ' analyze -c '. realpath(__DIR__.'/../cross-repo-unused-methods-recorder.neon') .' '. escapeshellarg($argv[1]) . ' --debug --xdebug';
-echo $cmd;
-system($cmd);
+$output = \staabm\CrossRepoUnusedPublic\execCmd($cmd, $stderrOutput, $exitCode);
+
+echo $output;
+fwrite(STDERR, $stderrOutput);
+exit($exitCode);
